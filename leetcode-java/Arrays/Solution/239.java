0d6b571c3b25882de4859c4b912b0e99c23a41e6
LiuYikang <Lyiker@outlook.com>
2019-03-19 00:27:15 +0800

    add 239 solution

---
+++ leetcode-java/Arrays/Solution/239.java
@@ -0,0 +1,57 @@
+/**
+ * 239. Sliding Window Maximum
+
+Given an array nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position. Return the max sliding window.
+
+Example:
+
+Input: nums = [1,3,-1,-3,5,3,6,7], and k = 3
+Output: [3,3,5,5,6,7] 
+Explanation: 
+
+Window position                Max
+---------------               -----
+[1  3  -1] -3  5  3  6  7       3
+ 1 [3  -1  -3] 5  3  6  7       3
+ 1  3 [-1  -3  5] 3  6  7       5
+ 1  3  -1 [-3  5  3] 6  7       5
+ 1  3  -1  -3 [5  3  6] 7       6
+ 1  3  -1  -3  5 [3  6  7]      7
+Note: 
+You may assume k is always valid, 1 ≤ k ≤ input array's size for non-empty array.
+
+Follow up:
+Could you solve it in linear time?
+ */
+
+/**
+ * Solution 1
+ * Heap
+ * time O(NlogK) space O(K)
+ */
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if (nums == null || nums.length == 0 || k == 0) {
+            return new int[k];
+        }
+        
+        Queue<Integer> pq = new PriorityQueue<>(k, (Comparator<Integer>)(a, b) -> a > b ? -1 : 1);
+        for (int i = 0; i < k; i++) {
+            pq.add(nums[i]);
+        }
+        
+        List<Integer> res = new ArrayList<>();
+        res.add(pq.peek());
+        
+        for (int i = k; i < nums.length; i++) {
+            pq.remove(nums[i - k]);
+            pq.add(nums[i]);
+            res.add(pq.peek());
+        }
+        int[] ret = new int[res.size()];
+        for (int i = 0; i < res.size(); i++) {
+            ret[i] = res.get(i);
+        }
+        return ret;
+    }
+}
