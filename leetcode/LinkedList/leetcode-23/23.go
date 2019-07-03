ca1dbe39ff148d9de8511e609a5de908b97abc5a
LiuYikang <Lyiker@outlook.com>
2019-07-03 23:30:45 +0800

    go heap solution solve 23

--- leetcode/LinkedList/leetcode-23/23.go
+++ leetcode/LinkedList/leetcode-23/23.go
@@ -1,5 +1,7 @@
 package linkedlist
 
+import "container/heap"
+
 // ListNode Definition for singly-linked list.
 type ListNode struct {
 	Val  int
@@ -53,4 +55,57 @@ func mergeList(head1, head2 *ListNode) *ListNode {
 	}
 
 	return dummy.Next
+}
+
+// solution 2
+func mergeKLists2(lists []*ListNode) *ListNode {
+	if lists == nil || len(lists) == 0 {
+		return nil
+	}
+
+	hp := ListNodeHeap{}
+	for i := 0; i < len(lists); i++ {
+		if lists[i] == nil {
+			continue
+		}
+		hp = append(hp, lists[i])
+	}
+
+	dummy := &ListNode{0, nil}
+	p := dummy
+	heap.Init(&hp)
+	for len(hp) != 0 {
+		tmp := heap.Pop(&hp).(*ListNode)
+		p.Next = tmp
+		p = p.Next
+		if tmp.Next != nil {
+			heap.Push(&hp, tmp.Next)
+		}
+	}
+	return dummy.Next
+}
+
+type ListNodeHeap []*ListNode
+
+func (p ListNodeHeap) Len() int {
+	return len(p)
+}
+
+func (p ListNodeHeap) Swap(i, j int) {
+	p[i], p[j] = p[j], p[i]
+}
+
+func (p ListNodeHeap) Less(i, j int) bool {
+	return p[i].Val < p[j].Val
+}
+
+func (p *ListNodeHeap) Push(h interface{}) {
+	*p = append(*p, h.(*ListNode))
+}
+
+func (p *ListNodeHeap) Pop() (x interface{}) {
+	n := len(*p)
+	x = (*p)[n-1]
+	*p = (*p)[:n-1]
+	return x
 }
